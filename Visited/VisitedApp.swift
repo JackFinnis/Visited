54990d0fabac7d8e21fefc52dda12f843f2042b7
Jack Finnis <jack.finnis@icloud.com>
2023-05-06 21:43:38 +0100

    Initial Commit

---
+++ Visited/VisitedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisitedApp.swift
+//  Visited
+//
+//  Created by Jack Finnis on 06/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct VisitedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
